37f3153b5915ef8e2a991de72f56ec62a2734147
Danny Peng <dannyw.peng@gmail.com>
2018-04-06 18:00:13 -0700

    heroku push changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+demo_user = User.create({first_name: "Gabriel", last_name: "Talavera", email: "gabe@gabe.com", password: "password"})
