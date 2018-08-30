86da55ecb9d6dcb1d772b80438ec696edfa9a6b3
Danny Peng <dannyw.peng@gmail.com>
2018-08-30 00:57:00 -0700

    restyled session form page

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -39,38 +39,55 @@ class SessionForm extends React.Component {
 
   render () {
     return (
-      <div className="session-form-container">
-        <form onSubmit={this.handleSubmit} className="session-form-box">
-          <div className="session-form">
-            <div className="session-text">
-              <h2>Sign In</h2>
-              {this.renderErrors()}
-              <br/>
-            </div>
-            <input
-              type="text"
-              value={this.state.email}
-              placeholder="Enter your email"
-              onChange={this.update('email')}
-              className="session-input"
+      <div className="session-form-page">
+        <nav className="signup-form-navbar">
+          <div className="signup-form-navbar-logo-container">
+            <img
+              onClick={()=>this.props.history.push('/')}
+              className="signup-form-navbar-logo"
+              src="https://s3-us-west-1.amazonaws.com/apertr-dev/photos/images/static+images/yeehaw.png"
             />
-            <br/>
-            <input
-              type="password"
-              value={this.state.password}
-              placeholder="Password"
-              onChange={this.update('password')}
-              className="session-input"
-            />
-            <br/>
-            <input type="submit" value="Sign in" className="session-submit" />
-            <br/>
-            <div className="session-text-bottom">
-              <p>Don't have an account?</p>
-              <div>{this.props.navLink}</div>
-            </div>
           </div>
-        </form>
+        </nav>
+        <div className="session-form-container">
+          <form onSubmit={this.handleSubmit} className="session-form-box">
+            <div className="session-form">
+              <div className="session-form-logo-container">
+                <img
+                  className="session-form-signin-logo"
+                  src="https://s3-us-west-1.amazonaws.com/apertr-dev/photos/images/static+images/yeehaw.png"
+                />
+              </div>
+              <div className="session-text">
+                <h2>Sign In</h2>
+                {this.renderErrors()}
+                <br/>
+              </div>
+              <input
+                type="text"
+                value={this.state.email}
+                placeholder="Enter your email"
+                onChange={this.update('email')}
+                className="session-input"
+              />
+              <br/>
+              <input
+                type="password"
+                value={this.state.password}
+                placeholder="Password"
+                onChange={this.update('password')}
+                className="session-input"
+              />
+              <br/>
+              <input type="submit" value="Sign in" className="session-submit" />
+              <br/>
+              <div className="session-text-bottom">
+                <p>Don't have an account?</p>
+                <div>{this.props.navLink}</div>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
