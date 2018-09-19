4524fd3960309a0bcc1a5ef4c11fa9a46d647d1c
Danny Peng <dannyw.peng@gmail.com>
2018-09-18 19:21:49 -0700

    fixed some css bugs with splash photo index items

--- frontend/components/photo/photo_index_item_splash.jsx
+++ frontend/components/photo/photo_index_item_splash.jsx
@@ -4,16 +4,18 @@ import { Link } from "react-router-dom";
 const PhotoIndexItemSplash = props => {
   return (
     <li className="photo-index-item-container-splash">
-      <div className="photo-author">
-        {props.photo.userFname} {props.photo.userLname}
-      </div>
-      <div className="photo-image-splash-container">
-        <Link to={`/photos/${props.photo.id}`}>
-          <img className="photo-image-splash" src={props.photo.image_url} />
-        </Link>
-      </div>
-      <div className="photo-title">
-        {props.photo.title}
+      <div className="photo-index-item-splash">
+        <div className="photo-author">
+          {props.photo.userFname} {props.photo.userLname}
+        </div>
+        <div className="photo-image-splash-container">
+          <Link to={`/photos/${props.photo.id}`}>
+            <img className="photo-image-splash" src={props.photo.image_url} />
+          </Link>
+        </div>
+        <div className="photo-title">
+          {props.photo.title}
+        </div>
       </div>
     </li>
   );
