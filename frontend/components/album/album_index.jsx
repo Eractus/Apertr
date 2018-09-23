118aab77d751552485b0806b11f8dabebede0eea
Danny Peng <dannyw.peng@gmail.com>
2018-09-23 02:01:26 -0700

    more css and component styling

--- frontend/components/album/album_index.jsx
+++ frontend/components/album/album_index.jsx
@@ -5,16 +5,19 @@ import AlbumIndexItem from './album_index_item';
 class AlbumIndex extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { firstLoad: true }
   }
 
   componentDidMount() {
-    this.props.fetchAlbums();
+    this.props.fetchAlbums().then(() => this.setState({ firstLoad: false }));
   }
 
   render() {
-    if (!this.props.albums) {
+    if (this.state.firstLoad) {
       return (
-        <div>Loading...</div>
+        <div className="album-index-loading">
+          <p>Loading...</p>
+        </div>
       )
     }
 
@@ -26,30 +29,14 @@ class AlbumIndex extends React.Component {
         deleteAlbum={this.props.deleteAlbum} />
       );
     });
-    const newAlbum = <Link to="/albums/new" className="album-index-new-album">new album</Link>;
-    if (albums.length === 0) {
-      return (
-        <div className="no-albums">
-          <div className="no-albums-message">
-            <p>You have no albums yet! Please create a</p>
-            {newAlbum}
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div className="album-index-container">
-          <div className="album-index-header">
-            <h1>Your Albums (</h1>
-            {newAlbum}
-            <h1>)</h1>
-          </div>
-          <ul className="album-index-list">
-            {albums}
-          </ul>
-        </div>
-      );
-    }
+    return (
+      <div className="album-index-container">
+        <Link to="/albums/new" className="album-index-new-album">New album</Link>
+        <ul className="album-index-list">
+          {albums}
+        </ul>
+      </div>
+    );
   }
 }
 
--- frontend/components/album/album_index_item.jsx
+++ frontend/components/album/album_index_item.jsx
@@ -4,19 +4,17 @@ import { Link } from 'react-router-dom';
 const AlbumIndexItem = props => {
   return (
     <li>
-      <div className="album-index-image-container">
-        <div className="album-index-image">
-          <Link to={`/albums/${props.album.id}`}>
-            <img src={Object.values(props.album.photos)[0].image_url} />
+      <div className="album-index-item-container">
+        <Link to={`/albums/${props.album.id}`}>
+          <img className="album-index-image" src={Object.values(props.album.photos)[0].image_url} />
+        </Link>
+        <div className="album-index-title">
+          {props.album.title}
+        </div>
+        <div className="album-index-delete">
+          <Link to="/albums" onClick={() => props.deleteAlbum(props.album.id)}>
+            X
           </Link>
-          <div className="album-index-title">
-            {props.album.title}
-          </div>
-          <div className="album-index-delete">
-            <Link to="/albums" onClick={() => props.deleteAlbum(props.album.id)}>
-              X
-            </Link>
-          </div>
         </div>
       </div>
     </li>
