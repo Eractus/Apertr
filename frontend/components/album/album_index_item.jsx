8acf9811463bfcfe499fb972e70475e414084fa8
Danny Peng <dannyw.peng@gmail.com>
2018-09-23 17:54:47 -0700

    some styling all around app

--- frontend/components/album/album_index_item.jsx
+++ frontend/components/album/album_index_item.jsx
@@ -3,19 +3,17 @@ import { Link } from 'react-router-dom';
 
 const AlbumIndexItem = props => {
   return (
-    <li>
-      <div className="album-index-item-container">
-        <Link to={`/albums/${props.album.id}`}>
-          <img className="album-index-image" src={Object.values(props.album.photos)[0].image_url} />
+    <li className="album-index-item-container">
+      <Link to={`/albums/${props.album.id}`}>
+        <img className="album-index-image" src={Object.values(props.album.photos)[0].image_url} />
+      </Link>
+      <div className="album-index-title">
+        {props.album.title}
+      </div>
+      <div className="album-index-delete">
+        <Link to="/albums" onClick={() => props.deleteAlbum(props.album.id)}>
+          X
         </Link>
-        <div className="album-index-title">
-          {props.album.title}
-        </div>
-        <div className="album-index-delete">
-          <Link to="/albums" onClick={() => props.deleteAlbum(props.album.id)}>
-            X
-          </Link>
-        </div>
       </div>
     </li>
   );
