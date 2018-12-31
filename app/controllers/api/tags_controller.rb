ea4fe79b869a2ec7e24702ba6928d54e416729ce
Danny Peng <dannyw.peng@gmail.com>
2018-12-31 14:17:36 -0800

    comments/documentation

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -1,5 +1,6 @@
 class Api::TagsController < ApplicationController
   def create
+    # when a user addes a Tag word to the photo, first check if the Tag word they're adding already exists so we don't create duplicates; afterwards, create a new PhotoTag using the id's of the photo and tag instances
     @tag = Tag.find_by(word: params[:tag][:word])
     if @tag == nil
       @tag = Tag.new(tag_params)
@@ -20,6 +21,7 @@ class Api::TagsController < ApplicationController
   end
 
   def destroy
+    # when a user is deleting a Tag word from a Photo, they are really deleting the row entry in the PhotoTag joins table that's connecting the Tag instance with the Photo instance - we don't want to delete the Tag instance itself as it would remove the Tag word from any Photo that has a PhotoTag with this Tag word as well
     @tag = Tag.find(params[:id])
     photo_id = params[:photoId]
     photo_tag = PhotoTag.find_by(tag_id: @tag.id, photo_id: photo_id)
